b23a1d4d034d8a4f9a1f25190ec9910b5940a452
mrma2498 <mrodriguesaraujo@ipvc.pt>
2021-05-24 15:49:43 +0100

    Ligação com o projeto feita

--- app/src/main/java/ipvc/estg/tp2/MainActivity.kt
+++ app/src/main/java/ipvc/estg/tp2/MainActivity.kt
@@ -2,12 +2,40 @@ package ipvc.estg.tp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ipvc.estg.tp2.model.Produto
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //loadProdutos()
     }
 
     //teste
+    private fun loadProdutos(){
+        val docs = Firebase.firestore.collection("produtos")
+
+        Log.d("MARIA",docs.toString())
+
+
+        docs.addSnapshotListener{ snapshot, e ->
+
+            val produtos = mutableListOf<Produto>()
+            for (documento in snapshot!!.documents){
+                val produto = Produto(
+                documento.id,
+                "${documento.data?.get("nome")}",
+                ""
+                )
+                produtos += produto
+            }
+            Log.d("MARIA",produtos.toString())
+
+        }
+
+    }
 }
---
+++ app/src/main/java/ipvc/estg/tp2/model/Produto.kt
@@ -0,0 +1,7 @@
+package ipvc.estg.tp2.model
+
+data class Produto(
+    val id: String,
+    val nome: String,
+    val disponivel: String
+)
