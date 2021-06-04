088c564fb634b9bd3b0727d9302d137f506d0420
mrma2498 <mrodriguesaraujo@ipvc.pt>
2021-06-04 16:07:59 +0100

    Obtém lista de produtos

--- app/src/main/java/ipvc/estg/tp2/MainActivity.kt
+++ app/src/main/java/ipvc/estg/tp2/MainActivity.kt
@@ -8,12 +8,39 @@ import com.google.firebase.ktx.Firebase
 import ipvc.estg.tp2.model.Produto
 
 class MainActivity : AppCompatActivity() {
+
+    val produtos = mutableListOf<Produto>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        getListaProdutos();
     }
 
 
+    /**Obtém lista de produtos existentes no sistema e a sua disponibilidade.*/
+    private fun getListaProdutos(){
+
+        val docs = Firebase.firestore.collection("produtos")
+
+        docs.addSnapshotListener{ snapshot, e ->
+
+            for (documento in snapshot!!.documents){
+                val produto = Produto(
+                        documento.id,
+                        "${documento.data?.get("nome")}",
+                        documento.data?.getValue("disponivel") as Boolean
+
+                )
+                produtos += produto
+            }
+            for (p in produtos){
+                Log.d("MARIA",p.nome + p.disponivel + "\n")
+            }
+
+
+        }
+
+    }
 }
