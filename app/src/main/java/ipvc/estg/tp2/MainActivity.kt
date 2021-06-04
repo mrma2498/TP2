04d028ba059c549093d8c740eae548298f0bec55
mrma2498 <mrodriguesaraujo@ipvc.pt>
2021-06-04 17:11:26 +0100

    Obtém lista de parques de estacionamento

--- app/src/main/java/ipvc/estg/tp2/MainActivity.kt
+++ app/src/main/java/ipvc/estg/tp2/MainActivity.kt
@@ -5,19 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.type.LatLng
-import ipvc.estg.tp2.model.Parque
-import ipvc.estg.tp2.model.Produto
+
 
 class MainActivity : AppCompatActivity() {
-
-    val produtos = mutableListOf<Produto>()
-    val parques = mutableListOf<Parque>()
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,54 +22,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-
-        //getListaProdutos();
-        getListaParques()
     }
 
-
-    /**Obtém lista de produtos existentes no sistema e a sua disponibilidade.*/
-    private fun getListaProdutos(){
-
-        val docs = Firebase.firestore.collection("produtos")
-
-        docs.addSnapshotListener{ snapshot, e ->
-
-            for (documento in snapshot!!.documents){
-                val produto = Produto(
-                        documento.id,
-                        "${documento.data?.get("nome")}",
-                        documento.data?.getValue("disponivel") as Boolean
-                )
-                produtos += produto
-            }
-        }
-
-    }
-
-
-    /**Obtém a lista de parques de estacionamento.
-     * NOTA: Para aceder às coordenadas -> localizacao.latitude ou localizacao.longitude
-     *
-     * */
-    private fun getListaParques(){
-
-        val docs = Firebase.firestore.collection("parques")
-
-        docs.addSnapshotListener{ snapshot, e ->
-
-            for (documento in snapshot!!.documents){
-                val parque = Parque(
-                        documento.id.toInt(),
-                        "${documento.data?.get("nome")}",
-                        documento.data?.getValue("localizacao") as GeoPoint,
-                        documento.data?.getValue("livre") as Boolean
-
-                )
-                parques += parque
-            }
-
-        }
-    }
 }
--- app/src/main/java/ipvc/estg/tp2/MapsActivity.kt
+++ app/src/main/java/ipvc/estg/tp2/MapsActivity.kt
@@ -9,10 +9,17 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ipvc.estg.tp2.model.Parque
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    /**Variável que contém a lista dos parques de estacionamento*/
+    val parques = mutableListOf<Parque>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        getListaParques();
     }
 
     /**
@@ -39,5 +49,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+    }
+
+
+    /**Obtém a lista de parques de estacionamento.
+     * NOTA: Para aceder às coordenadas -> localizacao.latitude ou localizacao.longitude
+     *
+     * */
+    private fun getListaParques(){
+
+        val docs = Firebase.firestore.collection("parques")
+
+        docs.addSnapshotListener{ snapshot, e ->
+
+            for (documento in snapshot!!.documents){
+                val parque = Parque(
+                    documento.id.toInt(),
+                    "${documento.data?.get("nome")}",
+                    documento.data?.getValue("localizacao") as GeoPoint,
+                    documento.data?.getValue("livre") as Boolean
+
+                )
+                parques += parque
+            }
+
+        }
     }
 }
