b6ecd156a2a30ef724c47b7a160b0266df7624b3
Renato Rocha <renato.pr99@gmail.com>
2021-06-08 18:37:24 +0100

    Rotas com waypoints

--- app/src/main/java/ipvc/estg/tp2/MapsActivity.kt
+++ app/src/main/java/ipvc/estg/tp2/MapsActivity.kt
@@ -1,8 +1,17 @@
 package ipvc.estg.tp2
 
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,17 +19,27 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.firestore.GeoPoint
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import ipvc.estg.tp2.model.Parque
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import java.io.IOException
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-    /**Variável que contém a lista dos parques de estacionamento*/
-    val parques = mutableListOf<Parque>()
+
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +49,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        //inicialização fusedLocationClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        getListaParques();
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+                var loc = LatLng(lastLocation.latitude, lastLocation.longitude)
+                LocationChanged(lastLocation)
+
+
+
+
+
+
+            }
+        }
+
+        createLocationRequest()
     }
 
     /**
@@ -45,41 +81,204 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        setUpMap()
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
+        //mMap.isMyLocationEnabled = true
+
+        val location1 = LatLng(13.0356745, 77.5881522)
+        mMap.addMarker(MarkerOptions().position(location1).title("My location"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1, 5f))
+
+        val location2 = LatLng(9.89, 78.11)
+        mMap.addMarker(MarkerOptions().position(location2).title("Madurai"))
+
+        val location3 = LatLng(13.029727, 77.5933021)
+        mMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+
+        val URL = getDirectionURL(location1, location2)
+        GetDirection(URL).execute()
+    }
+
+    fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
+        Log.d("CCCorigin", " ${origin.latitude}, ${origin.longitude}")
+        Log.d("CCCdestination", " ${dest.latitude}, ${dest.longitude}")
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&waypoints=via:13.029727,77.5933021&sensor=false&mode=driving&key=AIzaSyDs95Q8wVFlwO1pVFJ7gxR-XnpFLKpUGMQ"
+    }
+
+    private inner class GetDirection(val url : String) : AsyncTask<Void,Void,List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body()!!.string()
+            Log.d("GoogleMap" , " data : $data")
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,GoogleMapDTO::class.java)
+
+                val path =  ArrayList<LatLng>()
+
+                for (i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
+//                    path.add(startLatLng)
+//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+            mMap.addPolyline(lineoption)
+        }
+    }
+
+    public fun decodePolyline(encoded: String): List<LatLng> {
+
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5), (lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
     }
 
 
-    /**Obtém a lista de parques de estacionamento.
-     * NOTA: Para aceder às coordenadas -> localizacao.latitude ou localizacao.longitude
-     *
-     * */
-    private fun getListaParques(){
 
-        val docs = Firebase.firestore.collection("parques")
 
-        docs.addSnapshotListener{ snapshot, e ->
 
-            for (documento in snapshot!!.documents){
-                val parque = Parque(
-                    documento.id.toInt(),
-                    "${documento.data?.get("nome")}",
-                    documento.data?.getValue("localizacao") as GeoPoint,
-                    documento.data?.getValue("livre") as Boolean
 
-                )
-                parques += parque
-            }
-            Log.d("maria",parques.toString())
 
-            /**Insere os markers no mapa aqui, como fizemos no primeiro trabalho.
-             *  Se for fora desta função a lista está vazia.
-             *  */
 
+
+
+
+
+
+    override fun onPause(){
+        super.onPause()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    public override fun onResume(){
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    private fun startLocationUpdates() {
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
         }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    fun setUpMap(){
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+
+            return
+        } else {
+            mMap.isMyLocationEnabled = true
+
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener(this) { location->
+                if(location != null) {
+                    lastLocation = location
+                    Toast.makeText(this@MapsActivity, lastLocation.toString(), Toast.LENGTH_SHORT).show()
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
+    }
+
+    private fun createLocationRequest(){
+        locationRequest = LocationRequest()
+        locationRequest.interval = 1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    public fun setAddress(adress: Address) {
+        if(adress != null) {
+            if(adress.getAddressLine(0) != null) {
+                Log.d("LLL", ""+ adress.getAddressLine(0) +"")
+
+            }
+            if(adress.getAddressLine(1) != null) {
+                Log.d("LLL", ""+ adress.getAddressLine(1) +"")
+
+            }
+        }
+    }
+
+    private fun LocationChanged(p0: Location) {
+        Log.d("LLL", "changed!!")
+
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var Adress: List<Address>? = null
+
+        try{
+            Adress = geoCoder.getFromLocation(p0.latitude, p0.longitude,1)
+        }catch (e: IOException) {
+            e.printStackTrace()}
+        setAddress(Adress!![0])
     }
 }
