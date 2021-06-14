c7892d5cca9c554dbd7b0f5910c9ce140d60f366
Renato Rocha <renato.pr99@gmail.com>
2021-06-14 15:15:27 +0100

    update

---
+++ app/src/main/java/ipvc/estg/tp2/model/Loja.kt
@@ -0,0 +1,14 @@
+package ipvc.estg.tp2.model
+
+import com.google.firebase.firestore.GeoPoint
+
+data class Loja(
+
+    val id: Int,
+    val nome: String,
+    val localizacao: GeoPoint,
+    val email: String,
+    val password: String,
+    val produtos: List<Produto>
+
+)
